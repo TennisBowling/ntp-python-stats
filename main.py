70b5e0897a6492d3d4be45b1fded0426347d4c95
TennisBowling <tennisbowling@tennisbowling.com>
2021-11-03 17:29:50 -0700

    good, works.

--- main.py
+++ main.py
@@ -16,38 +16,39 @@ import asyncio
 # sudo ln -s /usr/sbin/tcpdump /usr/bin/tcpdump
 
 async def getdata():
-  #connect to database, remember credentials and hostname and database
-  db = await asyncpg.connect('postgresql://postgres@192.168.86.37/ntp')
-  #start the tcpdump process (-n no reverse lookup)
-  p = subprocess.Popen(('sudo', 'tcpdump', 'dst', 'port', '123', '-n', '-l'), stdout=subprocess.PIPE)
-
-  while True:
-    for row in iter(p.stdout.readline, b''):
-      data = str(row.rstrip())
-      #split output into list
-      splitted = data.split()
-
-      #sometimes something weird happens, therefor test the output
-      try:
-        type = splitted[6][:-1]
-      except IndexError:
-       pass
-
-      #get the values we want
-      sourceipsplit = splitted[2].split('.',4)
-      sourceip = ".".join(sourceipsplit[0:-1])
-      destinationip = splitted[4][:-1]
-      protocolversion = splitted[5][:-1]
-
-      #we are only interested in the client traffic
-      if type == 'Client':
-        ip = sourceip
-
-        #parse this into integers for MySQL
-        #construct SQL and replace (delete if exist and insert new)
-        sql = """REPLACE INTO clients(time, ip) VALUES (now(), $1)"""
-        sqlip = struct.unpack("!I", socket.inet_aton(ip))[0]
-        db.execute(sql, sqlip)
+    #connect to database, remember credentials and hostname and database
+    db: asyncpg.Connection = await asyncpg.connect('yours')
+    #start the tcpdump process (-n no reverse lookup)
+    cache = list()
+    p = subprocess.Popen(('sudo', 'tcpdump', 'dst', 'port', '123', '-n', '-l'), stdout=subprocess.PIPE)
+   
+    while True:
+        for row in iter(p.stdout.readline, b''):
+            data = str(row.rstrip())
+            #split output into list
+            splitted = data.split()
+       
+            #sometimes something weird happens, therefor test the output
+            try:
+              type = splitted[6][:-1]
+            except IndexError:
+             pass
+       
+            #get the values we want
+            sourceipsplit = splitted[2].split('.',4)
+            sourceip = ".".join(sourceipsplit[0:-1])
+            destinationip = splitted[4][:-1]
+            protocolversion = splitted[5][:-1]
+       
+            #we are only interested in the client traffic
+            if type == 'Client':
+              ip = sourceip
+       
+              cache.append([ip, 0])
+              if len(cache) > 1500:
+                await db.executemany("""INSERT INTO clients VALUES ($1, $2) ON CONFLICT (ip) DO UPDATE SET "amount" = clients.amount + 1 WHERE clients.ip = $1""", cache)
+                print(f'Pushed {len(cache)} ips')
+                cache.clear()
 
 
 def run_getdata():
